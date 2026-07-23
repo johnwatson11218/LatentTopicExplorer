37918b338ad1527155bf5a37c2bcf8b2987b5463
johnwatson11218 <john.watson@gmail.com>
2026-07-23 14:08:34 -0700

    working on making links work to download pdf files

--- postgresql_version/web.py
+++ postgresql_version/web.py
@@ -1,7 +1,8 @@
 import psycopg2
 from psycopg2.extensions import connection as PGConnection
-
+from collections import defaultdict
 from flask import Flask, render_template
+from functools import lru_cache
 
 app = Flask( __name__ ) 
 
@@ -24,11 +25,33 @@ def get_db_connection(
 @app.route( "/" )
 def hello_world():
     conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute( 'select id, filename from documents order by id desc ' )
-    rows = cur.fetchall()
-    data_items = [ ( r[0], r[1] ) for r in rows ]
-    return render_template( 'index.html', data_items = data_items )
+    data_items = get_documents( conn )
+    topics_and_documents = get_topics_and_documents( conn )
+    conn.close()
+    return render_template( 'index.html', data_items = data_items, topics_and_documents = topics_and_documents )
 
+
+@lru_cache(maxsize=128)
+def get_documents( conn ):
+    cur = conn.cursor()
+    cur.execute( 'select id, filename from documents order by id  ' )
+    rows = cur.fetchall()
+    data_items = [ ( r[0], r[1]) for r in rows ]
+    cur.close()
+    return data_items
+
+@lru_cache(maxsize=128)
+def get_topics_and_documents( conn ):
+    cur = conn.cursor()
+    cur.execute( """
+                select c.label, d.filename from categories c, document_categories dc, documents d
+                    where c.id = dc.category_id and dc.document_id = d.id
+                    order by 1 
+                """)
+    rows = cur.fetchall()
+    topics_and_documents = defaultdict( list )
+    [ topics_and_documents[r[0]].append( r[1]) for r in rows ]        
+    cur.close()
+    return topics_and_documents
     
 
